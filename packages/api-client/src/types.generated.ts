f4568c88ea0ffbcb5382dc26d164d5487dab400a
Anton Sidorov aka anticodeguy <a@anticodeguy.com>
2026-09-02 12:51:48 +0700

    chore(1518): regenerate api-client after rebase onto #1740

--- packages/api-client/src/types.generated.ts
+++ packages/api-client/src/types.generated.ts
@@ -1716,6 +1716,22 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
+    "/v1/storefront/doctor/register": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get?: never;
+        put?: never;
+        post: operations["DoctorRegisterPublicController_register"];
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
 }
 export type webhooks = Record<string, never>;
 export interface components {
@@ -1784,6 +1800,19 @@ export interface components {
             tense: "upcoming" | "past";
             to: string;
             totalCount: number;
+        };
+        DoctorRegisterRequestDto: {
+            captchaToken?: string;
+            /** @default [] */
+            consent: {
+                purpose: string;
+                version: string;
+            }[];
+            /** Format: email */
+            email: string;
+            /** @constant */
+            medicalWorkerDeclaration: true;
+            password: string;
         };
         EligibleExpertUserListDto: {
             data: {
@@ -4539,6 +4568,27 @@ export interface operations {
                 content: {
                     "application/json": components["schemas"]["DoctorEventsFeedDto"];
                 };
+            };
+        };
+    };
+    DoctorRegisterPublicController_register: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody: {
+            content: {
+                "application/json": components["schemas"]["DoctorRegisterRequestDto"];
+            };
+        };
+        responses: {
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content?: never;
             };
         };
     };
